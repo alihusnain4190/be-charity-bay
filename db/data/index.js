c2059a429905c027f209a08121bd652ebf509a46
alihusnain4190 <alihusnain4190@gmail.com>
2021-04-15 01:54:09 +0100

    add ssl in connection file

--- db/data/index.js
+++ db/data/index.js
@@ -3,4 +3,5 @@ const ENV = process.env.NODE_ENV || 'development';
 const testData = require('./test-data/index');
 const devData = require('./development-data/index');
 const data = { development: devData, test: testData, production: devData };
+// const data = { test, development, production: development };
 module.exports = data[ENV];
